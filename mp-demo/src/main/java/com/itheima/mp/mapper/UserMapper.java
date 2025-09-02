3c12ca6acb45e691ef0c2ca45667524faa882fcf
hxkkry <1504941291@qq.com>
2025-09-02 23:51:40 +0800

    MySQL

--- mp-demo/src/main/java/com/itheima/mp/mapper/UserMapper.java
+++ mp-demo/src/main/java/com/itheima/mp/mapper/UserMapper.java
@@ -1,8 +1,10 @@
 package com.itheima.mp.mapper;
 
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.itheima.mp.domain.po.User;
 import org.apache.ibatis.annotations.Param;
+import org.springframework.core.Constants;
 
 import java.util.List;
 
@@ -17,4 +19,6 @@ public interface UserMapper extends BaseMapper<User> {
     User queryUserById(@Param("id") Long id);
 
     List<User> queryUserByIds(@Param("ids") List<Long> ids);
+
+    void updateBalanceByIds(@Param("ew")UpdateWrapper<User> updateWrapper,@Param("amount")int amount);
 }
